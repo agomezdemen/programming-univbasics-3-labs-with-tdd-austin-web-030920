78d60f82f6d0feac8a5dddf65fac468ef57e40fa
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 02:59:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,6 @@
 first_number = 0
 second_number = 1
-
-def sum
-  puts first_number + second_number
-end
-
-def difference
-  puts first_number - second_number
-end
-
-def quotient
-  puts first_number / second_number
-end
-
-def product
-  puts first_number * second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
